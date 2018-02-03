ac2b1e57493ef852e3f90975208c75237371e4a9
Aldebaro Klautau <aldebaro@LAPTOP-8R7EBD20.localdomain>
2018-02-03 10:53:51 -0600

    Something

--- matlab/simulationPlanning.m
+++ matlab/simulationPlanning.m
@@ -2,13 +2,13 @@
 %%collected by Pedro using our fastest computer.
 
 rtDegreeRes=[1 0.25]; %InSite ray tracing (rt) resolution in degrees
-diffusionScattering=0; %enabled or not
+diffusionScattering=1; %enabled or not
 if diffusionScattering==1
     TstartSim=[66 198]; %time estimated by Pedro for 1 Rx (seconds)
     TperRxSim=[5 10]; %time estimated by Pedro for adding an extra Rx (seconds)
 else %Pedro, should I divide by 2?????
-    TstartSim=[66 198]/2; %time estimated by Pedro for 1 Rx (seconds)
-    TperRxSim=[5 10]/2; %time estimated by Pedro for adding an extra Rx (seconds)
+    TstartSim=[66 198]/3; %time estimated by Pedro for 1 Rx (seconds)
+    TperRxSim=[5 10]/3; %time estimated by Pedro for adding an extra Rx (seconds)
 end
 chosenDegreeRes=1; %choose a resolution
 
@@ -17,8 +17,8 @@ vel=15 %m/s
 streetWidth=20 %meters
 d=tan(rtDegreeRes(chosenDegreeRes)*pi/180)*streetWidth; %distance, ortogonal to DSU
 Ts=d/vel %to given an idea
-Ts=40e-3; %assumed Ts
-Te=2;
+Ts=100e-3; %assumed Ts
+Te=5;
 numScenesPerEpisode=floor(Te/Ts) %num of scene per episode
 
 %% Evaluate processing time
