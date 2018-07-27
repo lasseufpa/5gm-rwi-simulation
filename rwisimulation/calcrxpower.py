3130fd6740e320d1faf451f349beb99a80cefd30
aldebaro <aldebaro.klautau@gmail.com>
2018-07-27 16:29:24 -0500

    Support to ULA

--- rwisimulation/calcrxpower.py
+++ rwisimulation/calcrxpower.py
@@ -3,14 +3,7 @@ import os
 import datetime
 
 import numpy as np
-
 from rwiparsing import P2mPaths
-
-"""TODO: maybe we could insert in the DNN a snapshot of street situation and ask for the best path for each receiver
-to map this as a reinforced learning we could give a low reward when two users are allocated the same tx element
-and a high reward when a good fairness is achieved
-"""
-
 
 def dft_codebook(dim):
     seq = np.matrix(np.arange(dim))
@@ -18,6 +11,26 @@ def dft_codebook(dim):
     w = np.exp(-1j * 2 * np.pi * mat / dim)
     return w
 
+def arrayFactorGivenAngleForULA(numAntennaElements,theta,normalizedAntDistance=0.5,angleWithArrayNormal=0):
+    '''
+    Calculate array factor for ULA for angle theta. If angleWithArrayNormal=0
+    (default),the angle is between the input signal and the array axis. In
+    this case when theta=0, the signal direction is parallel to the array
+    axis and there is no energy. The maximum values are for directions 90
+        and -90 degrees, which are orthogonal with array axis.
+    If angleWithArrayNormal=1, angle is with the array normal, which uses
+    sine instead of cosine. In this case, the maxima are for
+        thetas = 0 and 180 degrees.
+    References:
+    http://www.waves.utoronto.ca/prof/svhum/ece422/notes/15-arrays2.pdf
+    Book by Balanis, book by Tse.
+    '''
+    indices = np.arange(numAntennaElements)
+    if (angleWithArrayNormal == 1):
+        arrayFactor=np.exp(-1j*2*np.pi*normalizedAntDistance*indices * np.sin(theta))
+    else: #default
+        arrayFactor=np.exp(-1j*2*np.pi*normalizedAntDistance*indices * np.cos(theta))
+    return arrayFactor / np.sqrt(numAntennaElements) #normalize to have unitary norm
 
 def calc_rx_power(departure_angle, arrival_angle, p_gain, antenna_number, frequency=6e10):
     """This .m file uses a m*m SQUARE UPA, so the antenna number at TX, RX will be antenna_number^2.
@@ -78,26 +91,150 @@ def calc_rx_power(departure_angle, arrival_angle, p_gain, antenna_number, freque
     t1 = wt.conj().T * H * wr
     return t1
 
+def getNarrowBandUPAMIMOChannel(departure_angles, arrival_angles, p_gains, number_Tx_antennas, number_Rx_antennas, normalizedAntDistance=0.5):
+    """This .m file uses UPAs at both TX and RX.
+
+    - assumes one beam per antenna element
+
+    the first column will be the elevation angle, and the second column is the azimuth angle correspondingly.
+    p_gain will be a matrix size of (L, 1)
+    departure angle/arrival angle will be a matrix as size of (L, 2), where L is the number of paths
+
+    t1 will be a matrix of size (nt, nr), each
+    element of index (i,j) will be the received
+    power with the i-th precoder and the j-th
+    combiner in the departing and arrival codebooks
+    respectively
+
+    :param departure_angles: ((elevation angle, azimuth angle),) (L, 2) where L is the number of paths
+    :param arrival_angles: ((elevation angle, azimuth angle),) (L, 2) where L is the number of paths
+    :param p_gain: path gain (L, 1) where L is the number of paths
+    :param antenna_number: antenna number at TX, RX is antenna_number**2
+    :param frequency: default
+    :return:
+    """
+    departure_angles = np.deg2rad(departure_angles)
+    arrival_angles = np.deg2rad(arrival_angles)
+    #nt = number_Rx_antennas * number_Tx_antennas #np.power(antenna_number, 2)
+    m = np.shape(departure_angles)[0]
+    wt = dft_codebook(number_Tx_antennas)
+    wr = dft_codebook(number_Rx_antennas)
+    H = np.matrix(np.zeros((number_Tx_antennas, number_Rx_antennas)))
+
+    #TO DO: need to generate random phase and convert gains in complex-values
+    gain_dB = p_gains
+    path_gain = np.power(10, gain_dB/10)
+    antenna_range = np.arange(antenna_number)
+
+    def calc_omega(angle):
+        sin = np.sin(angle)
+        omegay = 2 * np.pi * normalizedAntDistance * sin[:, 1] * sin[:, 0]
+        omegax = 2 * np.pi * normalizedAntDistance * sin[:, 0] * np.cos(angle[:, 1])
+        return np.matrix((omegax, omegay))
+    departure_omega = calc_omega(departure_angles)
+    arrival_omega = calc_omega(arrival_angles)
+
+    def calc_vec_i(i, omega, antenna_range):
+        vec = np.exp(1j * omega[:,i] * antenna_range)
+        return np.matrix(np.kron(vec[1], vec[0]))
+    for i in range(m):
+        departure_vec = calc_vec_i(i, departure_omega, antenna_range)
+        arrival_vec = calc_vec_i(i, arrival_omega, antenna_range)
+        H = H + path_gain[i] * departure_vec.conj().T * arrival_vec
+    t1 = wt.conj().T * H * wr
+    return t1
+
+def getNarrowBandULAMIMOChannel(azimuths_tx, azimuths_rx, p_gainsdB, number_Tx_antennas, number_Rx_antennas, normalizedAntDistance=0.5, angleWithArrayNormal=0):
+    """This .m file uses ULAs at both TX and RX.
+
+    - assumes one beam per antenna element
+
+    the first column will be the elevation angle, and the second column is the azimuth angle correspondingly.
+    p_gain will be a matrix size of (L, 1)
+    departure angle/arrival angle will be a matrix as size of (L, 2), where L is the number of paths
+
+    t1 will be a matrix of size (nt, nr), each
+    element of index (i,j) will be the received
+    power with the i-th precoder and the j-th
+    combiner in the departing and arrival codebooks
+    respectively
+
+    :param departure_angles: ((elevation angle, azimuth angle),) (L, 2) where L is the number of paths
+    :param arrival_angles: ((elevation angle, azimuth angle),) (L, 2) where L is the number of paths
+    :param p_gaindB: path gain (L, 1) in dB where L is the number of paths
+    :param number_Rx_antennas, number_Tx_antennas: number of antennas at Rx and Tx, respectively
+    :return:
+    """
+    azimuths_tx = np.deg2rad(azimuths_tx)
+    azimuths_rx = np.deg2rad(azimuths_rx)
+    #nt = number_Rx_antennas * number_Tx_antennas #np.power(antenna_number, 2)
+    m = np.shape(azimuths_tx)[0] #number of rays
+    wt = dft_codebook(number_Tx_antennas)
+    wr = dft_codebook(number_Rx_antennas)
+    H = np.matrix(np.zeros((number_Rx_antennas, number_Tx_antennas)))
+
+    #TO DO: need to generate random phase and convert gains in complex-values
+    gain_dB = p_gainsdB
+    path_gain = np.power(10, gain_dB/10)
+    path_gain = np.sqrt(path_gain)
+    #recall that in the narrowband case, the time-domain H is the same as the
+    #frequency-domain H
+    for i in range(m):
+        #at and ar are row vectors (using Python's matrix)
+        at=np.matrix(arrayFactorGivenAngleForULA(number_Tx_antennas,azimuths_tx[i],normalizedAntDistance,angleWithArrayNormal))
+        ar=np.matrix(arrayFactorGivenAngleForULA(number_Rx_antennas,azimuths_rx[i],normalizedAntDistance,angleWithArrayNormal))
+        H = H + path_gain[i] * ar.conj().T * at #outer product of ar Hermitian and at
+    factor=(np.linalg.norm(path_gain)/np.sum(path_gain))*np.sqrt(number_Rx_antennas*number_Tx_antennas) #scale channel matrix
+    H *= factor #normalize for compatibility with Anum's Matlab code
+    dictionaryOperatedChannel = wr.conj().T * H * wt
+    #dictionaryOperatedChannel2 = wr.T * H * wt.conj()
+    return dictionaryOperatedChannel #return equivalent channel after precoding and combining
+
 if __name__ == '__main__':
 
-    RESULTS_DIR='/Users/psb/ownCloud/Projects/DNN Wireless/rwi-3d-modeling/restuls/run00000'
-
+    #RESULTS_DIR='/Users/psb/ownCloud/Projects/DNN Wireless/rwi-3d-modeling/restuls/run00000'
+    RESULTS_DIR='D:/insitedata/pc128/example_working/restuls/run00000'
+    #RESULTS_DIR='/mnt/d/insitedata/pc128/example_working/restuls/run00000'
+    antenna_number = 2
     BASE_DIR=os.path.dirname(os.path.realpath(__file__))
     P2MPATHS_FILE=os.path.join(RESULTS_DIR, 'study', 'model.paths.t001_01.r002.p2m')
-
+    print('Reading file', P2MPATHS_FILE)
     with open(P2MPATHS_FILE, 'rb') as infile:
 
         paths = P2mPaths(P2MPATHS_FILE)
-        departure_angle = paths.get_departure_angle_ndarray(1)
-        arrival_angle = paths.get_arrival_angle_ndarray(1)
-        p_gain = paths.get_p_gain_ndarray(1)
-        antenna_number = 4
-        print(arrival_angle.shape)
+        departure_angles = paths.get_departure_angle_ndarray(1)
+        arrival_angles = paths.get_arrival_angle_ndarray(1)
+        p_gainsdB = paths.get_p_gain_ndarray(1)
+
+        if True: #enable for debugging with fixed angles
+            ad=(np.pi/4)*180/np.pi  #in degrees, as InSite provides
+            aa=(3*np.pi/2)*180/np.pi
+            g=10
+            departure_angles = ad*np.ones(departure_angles.shape, departure_angles.dtype)
+            arrival_angles = aa*np.ones(arrival_angles.shape, arrival_angles.dtype)
+            p_gainsdB = g * np.ones(p_gainsdB.shape, p_gainsdB.dtype)
+
+        print(arrival_angles.shape)
+        azimuths_tx = departure_angles[:,1] #azimuth is 2nd column
+        azimuths_rx = arrival_angles[:,1]
 
         start = datetime.datetime.today()
-        t1_py = calc_rx_power(departure_angle, arrival_angle, p_gain, antenna_number)
-        stop = datetime.datetime.today()
+        t1_py = calc_rx_power(departure_angles, arrival_angles, p_gainsdB, antenna_number)
 
-        #print(np.mean(np.power(t1 - t1_py, 2)))
+        number_Rx_antennas = antenna_number * antenna_number
+        number_Tx_antennas = antenna_number * antenna_number
+        normalizedAntDistance = 0.5
+
+        t1 = getNarrowBandUPAMIMOChannel(departure_angles, arrival_angles, p_gainsdB, number_Tx_antennas, number_Rx_antennas, normalizedAntDistance=0.5)
+        t1 = np.abs(t1)
+        t2 = getNarrowBandULAMIMOChannel(azimuths_tx, azimuths_rx, p_gainsdB, number_Tx_antennas, number_Rx_antennas, normalizedAntDistance=0.5, angleWithArrayNormal=1)
+        print('MSE 1 = ', np.mean(np.power(np.abs(t1 - t1_py), 2)))
+        print('MSE 2 = ', np.mean(np.power(np.abs(t1 - t2), 2)))
+        #print(t2)
+        t2 = np.abs(t2)
+        (bestRxIndex, bestTxIndex) = np.unravel_index(np.argmax(t2, axis=None), t2.shape)
+        print('bestRxIndex: ', bestRxIndex, ' and bestTxIndex: ', bestTxIndex)
+
+        stop = datetime.datetime.today()
         print(stop - start)
-        print(t1_py)
+        #print(t1_py)
