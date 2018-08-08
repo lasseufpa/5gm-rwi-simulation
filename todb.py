b2ca373f3c6fd89f520945726e4eb5e69aeb48de
Aldebaro Klautau <aldebaro@LAPTOP-8R7EBD20.localdomain>
2018-08-07 20:00:06 -0500

    Added support to phase

--- todb.py
+++ todb.py
@@ -8,6 +8,7 @@ from rwisimulation.tfrecord import UnexpectedCarsWithAntennaChangeError, SceneNo
     EpisodeNotStartingFromZeroError
 from rwimodeling import objects
 from rwiparsing import P2mPaths
+from rwiparsing import P2mCir
 
 from rwisimulation.datamodel import save5gmdata as fgdb
 
@@ -18,11 +19,17 @@ session = fgdb.Session()
 sc_i = 0
 ep_i = 0
 episode = None
-for run_i in c.n_run:
-#for run_i in range(50, 100):
+#for run_i in c.n_run: # use the number of examples in config.py
+for run_i in range(50): # enable in case you have less examples than indicated in c.n_run
     run_dir = os.path.join(c.results_dir, c.base_run_dir_fn(run_i))
     object_file_name = os.path.join(run_dir, os.path.basename(c.dst_object_file_name))
     abs_paths_file_name = os.path.join(run_dir, os.path.basename(c.project_output_dir), c.paths_file_name)
+    abs_cir_file_name = abs_paths_file_name.replace("paths","cir") #name for the impulse response (cir) file
+    if os.path.exists(abs_cir_file_name) == False:
+        print('ERROR: could not find file ', abs_cir_file_name)
+        print('Did you ask InSite to generate the impulse response (cir) file?')
+        exit(-1)
+
     abs_simulation_info_file_name = os.path.join(run_dir, c.simulation_info_file_name)
     with open(abs_simulation_info_file_name) as infile:
         simulation_info = json.load(infile)
@@ -53,7 +60,9 @@ for run_i in c.n_run:
 
     with open(object_file_name) as infile:
         obj_file = objects.ObjectFile.from_file(infile)
+    print(abs_paths_file_name) #AK TODO
     paths = P2mPaths(abs_paths_file_name)
+    cir = P2mCir(abs_cir_file_name)
 
     scene = fgdb.Scene()
     # TODO read from InSite
@@ -77,6 +86,8 @@ for run_i in c.n_run:
                         receiver.mean_time_of_arrival=paths.get_mean_time_of_arrival(rec_i+1)
                         receiver.position = object.position
 
+                        phases = cir.get_phase_ndarray(rec_i+1) #get phases for all rays in degrees
+                        rayIndex = 0
                         for departure, arrival, path_gain, arrival_time, interactions_list in zip(
                                 paths.get_departure_angle_ndarray(rec_i+1),
                                 paths.get_arrival_angle_ndarray(rec_i+1),
@@ -89,8 +100,11 @@ for run_i in c.n_run:
                             ray.path_gain = path_gain
                             ray.time_of_arrival = arrival_time
                             ray.interactions = interactions_list
+                            ray.phaseInDegrees = phases[rayIndex]
 
                             receiver.rays.append(ray)
+                            rayIndex += 1 #update for next iteration
+                            #print('Ray = ', ray.path_gain, ' ', ray.phaseInDegrees) #to check
 
                     object.receivers.append(receiver)
                     rec_i += 1
